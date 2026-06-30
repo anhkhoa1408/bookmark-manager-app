875c0e11b9a3059a0f5516a5569bda60cb3098c6
anhkhoa1408 <akhoa981@gmail.com>
2026-06-30 17:34:08 +0700

    (chore) update secret

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -49,6 +49,7 @@ const firebaseAuthPlugin = (): BetterAuthPlugin => ({
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
   session: {
     expiresIn: 60 * 60 * 24 * 7,
   },
@@ -60,5 +61,4 @@ export const auth = betterAuth({
     },
   },
   plugins: [firebaseAuthPlugin(), tanstackStartCookies()],
-  trustedOrigins: ["http://localhost:3000", "https://bookmark-management.vercel.app"],
 });
